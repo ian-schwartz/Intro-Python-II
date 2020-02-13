f0bc182fa162df662940f453e640fd42dbbbb03d
Ian Schwartz <ian.schwartz23@gmail.com>
2020-02-13 13:40:07 -0800

    created item class, extended room, player inventory

--- src/player.py
+++ src/player.py
@@ -5,3 +5,10 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
